5dc132757c2ac85bc33f656f0c44d82b4f50fdf6
Babayew Allaberdi <first9name.last9name@gmail.com>
2020-05-03 04:10:14 +0300

    first upload

---
+++ js/script.js
@@ -0,0 +1,47 @@
+let loader = document.querySelector(".loader");
+let strings = [
+    "LIFE",
+    "a journey",
+    "hard",
+    "fun",
+    "easy",
+    "cool"
+];
+let loopNumber = 0;
+let count;
+let textFromArr;
+let textFromTag = "";
+let isDeleting = false;
+let timeout = 300;
+
+let init = () => {
+
+    count = loopNumber % strings.length;
+    textFromArr = strings[count];
+
+    if (!isDeleting) textFromTag = textFromArr.substring(0, textFromTag.length + 1);
+    else textFromTag = textFromArr.substring(0, textFromTag.length - 1);
+    
+    loader.innerHTML = textFromTag;
+
+    debugger;
+    
+    if (isDeleting) timeout = 200;
+
+    if (!isDeleting && textFromTag == textFromArr) {
+        isDeleting = true;
+        timeout = 1500;
+    }
+    else if (isDeleting && textFromTag == "") {
+        isDeleting = false;
+        loopNumber++;
+        timeout = 300;
+    }
+
+    setTimeout(() => {
+        init();
+    }, timeout);
+
+}
+
+init();
